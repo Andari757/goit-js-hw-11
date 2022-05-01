ffe8ed0809f6a07084e9a5cdd2a01175a289414e
Andari757 <matviyskrypka@gmail.com>
2022-05-01 19:42:26 +0300

    1

--- src/render.js
+++ src/render.js
@@ -11,8 +11,7 @@ export function renderGallery(r, i,  gallery,loadMore) {
     return
   }  
 
-  const markup = r.data.hits.map((e) => `<a class="item" href = "${e.largeImageURL}"><img height="300px" src="${e.webformatURL}" alt="${e.tags}" loading="lazy" /><div class="info"><p class="info-item"><b>Likes</b><br>${e.likes}</p><p class="info-item"><b>Views</b><br>${e.views}</p><p class="info-item"><b>Comments</b><br>${e.comments}</p><p class="info-item"><b>Downloads</b><br>${e.downloads}</p></div></a>`).join("")
-  gallery.insertAdjacentHTML("beforeend", markup)
+  renderHtml(r,gallery)
 
   lightbox.refresh()
 
@@ -21,4 +20,8 @@ export function renderGallery(r, i,  gallery,loadMore) {
   }
   totalHitsNotify(r,i,loadMore)
   smoothScroll(i)
+}
+function renderHtml(r, gallery) {
+  const markup = r.data.hits.map((e) => `<a class="item" href = "${e.largeImageURL}"><img height="300px" src="${e.webformatURL}" alt="${e.tags}" loading="lazy" /><div class="info"><p class="info-item"><b>Likes</b><br>${e.likes}</p><p class="info-item"><b>Views</b><br>${e.views}</p><p class="info-item"><b>Comments</b><br>${e.comments}</p><p class="info-item"><b>Downloads</b><br>${e.downloads}</p></div></a>`).join("")
+  gallery.insertAdjacentHTML("beforeend", markup)
 }
